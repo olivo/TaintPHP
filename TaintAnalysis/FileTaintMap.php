bf88dc4992dce70c01eb8b93b63ac31d791b2e5b
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-02-15 22:32:52 -0600

    Added Class for representing taint of an entire file

---
+++ TaintAnalysis/FileTaintMap.php
@@ -0,0 +1,12 @@
+<?php
+
+// Class that contains all the taint information for a file,
+// including its main function and auxiliary functions.
+
+class FileTaintMap {
+
+      private $MainTaintMap = null;
+      private $FunctionTaintMaps = array();
+}
+
+?>
