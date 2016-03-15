88a72ae2de6499f3fbc39fffa822b3ab55b7305f
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-03-15 00:13:54 -0500

    Created class FunctionSignatureMap

---
+++ CFG/FunctionSignatureMap.php
@@ -0,0 +1,32 @@
+<?php
+// Represents a map from function names to potential function signature targets.
+class FunctionSignatureMap {
+
+      private $map = array();
+
+      public function __construct() {
+      	     $map = array();
+      }
+
+      public function get($functionName) {
+      	     return $this->map[(string)$functionName];
+      }
+      
+      public function contains($functionName) {
+      	     return isset($this->map[(string)$functionName]);
+      }
+
+      public function add($functionSignature) {
+      	     if(!$this->map->contains($functionSignature->getFunctionName())) {
+	         $this->map[$functionSignature->getFunctionName()] = array();
+	     }
+	     $this->map[$functionSignature->getFunctionName()][] = $functionSignature;
+      }
+
+      public function addAll($functionSignatures) {
+      	     foreach($functionSignatures as $name => $signature) {
+	         $this->add($signature);
+	     }
+      }
+}
+?>
